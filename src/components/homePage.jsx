d3226dffe3f0817b3e6de0fa376321e03d281735
Olivia Wilson-Simmonds <heavenwilson210@gmail.com>
2025-07-01 15:04:50 -0400

    All homepage routing completed

---
+++ src/components/homePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Homepage =()=>
+{
+    return (
+        <div>
+            <h2>Welcome to  Student-Campus Management App</h2>
+            <p>Select Campuses or Students from the nav above</p>
+        </div>
+    );
+};
+
+export default Homepage;
