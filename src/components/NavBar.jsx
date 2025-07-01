cf6de1ffeac7b9f7039a0ed2ab9fef3da5e896e3
PKhant78 <phonekhantkyawswa555@gmail.com>
2025-07-01 14:29:05 -0400

    Added homepage tab in navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./NavBarStyles.css";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
-  return <nav className="navbar">{/* Some navbar stuff goes here */}</nav>;
+  return (
+    <nav className="navbar">
+      <Link to="/" className="nav-link">
+        Home
+      </Link>
+    </nav>
+  );
 };
 
 export default NavBar;
