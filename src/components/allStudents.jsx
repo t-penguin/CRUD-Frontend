423739aabb9c92246da93348d39e63314424db97
Olivia Wilson-Simmonds <heavenwilson210@gmail.com>
2025-07-01 15:28:42 -0400

    Wip student page

--- src/components/allStudents.jsx
+++ src/components/allStudents.jsx
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const AllStudents = () => {
-  return (
-    <div>
-      <h2>All Students</h2>
-      <p>This is the students view.</p>
-    </div>
-  );
+  const [students,setStudents]= useState ([])
 };
+
+useEffect(()=>{
+    const fillerStudents = [
+        {id: 1, firstName:"Joseph",lastName:"Shaikh"},
+        {id: 2, firstName:"Olivia",lastName:"K"}
+    ];
+    setStudents(fillerStudents);
+},
+[]);
+
+return(
+    <div>
+        <h2>All Students</h2>
+        <Link to="/addStudent">🧑‍🎓Add Student</Link>
+        <ul>
+            {students.length=== 0 ?(
+                <p>No students available</p>
+            ):(
+                students.map((student)=>(
+                    <li key= {student.id}>
+                        {student.firstName} {student.lastName}
+                        <Link to={`/students/${student.id}`}>View Details</Link>
+                    </li>
+                ))
+            )}
+        </ul>
+    </div>
+);
+
+
 
 export default AllStudents;
