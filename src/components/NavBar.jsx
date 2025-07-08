eb4d9cb6fc2a69a19f7b2442574d41dbb047245a
Joseph Collado <sourpina@gmail.com>
2025-07-08 11:09:36 -0400

    Added logout link and logic

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,15 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import "./NavBarStyles.css";
+import axios from "axios";
 
-const NavBar = ({ loggedIn }) => {
+const NavBar = ({ loggedIn, setLoggedIn, API_URL }) => {
+  const logout = async () => {
+    await axios.post(`${API_URL}/auth/logout`, { withCredentials: true });
+    setLoggedIn(false);
+    alert("Successfully logged out");
+  }
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
@@ -10,9 +17,10 @@ const NavBar = ({ loggedIn }) => {
       </div>
       <div className="nav-links">
         <NavLink to="/campuses" className="nav-link">Campuses</NavLink>
-        <NavLink to="/students" className="nav-link"> Students</NavLink>
-        { !loggedIn && <NavLink to="/login" className="nav-link"> Login</NavLink> }
-        { !loggedIn && <NavLink to="/signup" className="nav-link"> Signup</NavLink> }
+        <NavLink to="/students" className="nav-link">Students</NavLink>
+        { !loggedIn && <NavLink to="/login" className="nav-link">Login</NavLink> }
+        { !loggedIn && <NavLink to="/signup" className="nav-link">Signup</NavLink> }
+        { loggedIn && <Link to="/" className="nav-link" onClick={logout}>Log out</Link>}
       </div>
     </nav>
   );
