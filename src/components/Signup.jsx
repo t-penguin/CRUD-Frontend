8ce12324c4290be51cccc995ecafbd01c427294b
Dagostocsc <Deborahagosto19@gmail.com>
2025-07-07 21:59:45 -0400

    css correction in progress

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-// import "./Signup.css";
+import "./Signup.css";
 
 const SignUp = () => {
   const navigate = useNavigate();
